39ca4612b825655650400046caebb9dea23f7b3e
TkachukSasha <sashatkachuk09@ukr.net>
2023-03-31 14:18:09 +0300

    #5 fix: issue with docker start

---
+++ backend/src/Services/Vacancies/Vacancies.Presentation/Infrastructure/AddDefaultServices.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+using Vacancies.Core;
+using Vacancies.Persistence;
+
+namespace Vacancies.Presentation.Infrastructure;
+
+public static class AddDefaultServices
+{
+    public static IServiceCollection AddDefault(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddCore();
+        services.AddMapper();
+        services.AddPersistence();
+
+        return services;
+    }
+
+    public static void UseDefault(this WebApplication app)
+    {
+        app.UseShared();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
--- backend/src/Services/Vacancies/Vacancies.Presentation/Program.cs
+++ backend/src/Services/Vacancies/Vacancies.Presentation/Program.cs
@@ -1,40 +1,12 @@
-using MapsterMapper;
-using MongoDB.Driver;
-using Shared.Abstractions.Primitives.Mongo;
-using Shared.Dal;
-using Vacancies.Core;
-using Vacancies.Core.Entities;
-using Vacancies.Core.Services;
-using Vacancies.Persistence;
+using Shared;
+using Vacancies.Presentation.Infrastructure;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication
+    .CreateBuilder(args)
+    .AddShared();
 
-// Add services to the container.
-
-
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddPersistence();
-builder.Services.AddCore();
-builder.Services.AddMapper();
+builder.Services.AddDefault();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-
-app.Run();
+app.UseDefault();

abf5f4df22b9ef1442c54f21f6d8b4d29b3ee87c
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-04-21 00:22:43 +0300

    rework

--- frontend/src/MainPage/Filters.js
+++ frontend/src/MainPage/Filters.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react'
 import styles from './Main.module.scss';
 
+
+
 const Filters = ({ showFilters }) => {
+    const [dateOfCreation, setDateOfCreation] = useState();
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+    const [minSalary, setMinSalary] = useState();
+    const [maxSalary, setMaxSalary] = useState();
+    const [skills, setSkills] = useState([]);
+    function salaryInput(event) {
+        const pattern = /[0-9]/;
+        const input = String.fromCharCode(event.charCode);
+        if (!pattern.test(input)) {
+            event.preventDefault();
+        }
+    }
+
+    function skillsInput(event) {
+        setSkills(prevSkills => [...prevSkills, document.getElementById('skillInput').value]);
+        console.log(skills);
+    }
+
+
+
     return (
         <div className={styles.modal}>
             <img onClick={showFilters} className={styles.close} width={35} src='./img/close.png' />
@@ -10,22 +33,44 @@ const Filters = ({ showFilters }) => {
                     <div>
                         <p>Enter salary:</p>
                     </div>
-                    <input className={styles.filtersInput} placeholder=' Minimum salary' />
-                    <input className={styles.filtersInput} placeholder=' Maximum salary' />
+                    <input onKeyPress={salaryInput} onChange={event => { setMinSalary(event.target.value) }} className={styles.filtersInput} placeholder=' Minimum salary' />
+                    <input onKeyPress={salaryInput} onChange={event => { setMaxSalary(event.target.value) }} className={styles.filtersInput} placeholder=' Maximum salary' />
                 </li>
+                {minSalary && maxSalary && (
+                    <p>You put: {minSalary} to {maxSalary} </p>
+                )}
                 <li>
                     <p>Enter date of creation:</p>
-                    <input className={styles.filtersInput} type='date' />
+                    <p><input value={dateOfCreation} onChange={(event) => { setDateOfCreation(event.target.value) }} className={styles.filtersInput} type='date' placeholder='Date of creation:' /></p>
+                    {dateOfCreation && (<p>You put: {dateOfCreation}</p>)}
                 </li>
-                <li>
+                <li className='d-flex'>
+                    <div>
+                        <p className='mr-40'>Enter from this date</p>
+                        <p>  <input value={startDate} onChange={(event) => { setStartDate(event.target.value) }} className={styles.filtersInput} type='date' placeholder='From that date' /></p>
+                    </div>
+                    <div>
+                        <p>Enter the following date</p>
+                        <p><input value={endDate} onChange={(event) => { setEndDate(event.target.value) }} className={styles.filtersInput} type='date' placeholder='To this date' /></p>
+                    </div>
+
+                </li>
+                {startDate && endDate && (
+                    <p>You put: {startDate} to {endDate} </p>
+                )}
+                <li className='d-flex align-center'>
                     <p>Enter skills</p>
-                    <input className={styles.filtersInput} placeholder=' Your skill:' />
+                    <input id="skillInput" className={styles.filtersInput} placeholder=' Your skill:' />
+                    <button className={styles.skills} onClick={skillsInput}>+</button>
                 </li>
-                <button className={styles.ok}>Оk</button>
+                {skills.length>0 && (
+                    <p>You put: {skills.join(',')} </p>
+                )}
+                <button onClick={showFilters} className={styles.ok}>Оk</button>
 
             </ul>
         </div>
     )
 }
 
-export default Filters
+export default Filters;

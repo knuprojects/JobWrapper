1c1ddc15565f6b510b646f8dee9e3517a48e9627
anechyt <anechytailknu@gmail.com>
2023-04-23 14:19:11 +0300

    add text extension

--- backend/src/Services/Vacancies/Vacancies.Core/Extensions.cs
+++ backend/src/Services/Vacancies/Vacancies.Core/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Globalization;
+using System.Text.RegularExpressions;
 using Vacancies.Core.Common.BackgroundServices;
 using Vacancies.Core.Common.Helpers;
 using Vacancies.Core.Services;
@@ -37,13 +38,23 @@ public static class Extensions
 
         if (text.Contains(WordOfYesterdayDay)) return DateTime.Today.AddDays(-1);
 
-        var monthDayText = text.Substring(0, 3);
+        string pattern = @"\d+\s\p{L}+";
+        Match match = Regex.Match(text, pattern);
 
-        var monthNumber = monthNameText?.GetMonthNumberFromMonthName();
+        if (match.Success)
+        {
+            string result = match.Value.Trim();
 
-        var monthDay = Convert.ToInt32(monthDayText);
+            var monthDayText = result.Substring(0, 3);
 
-        return new DateTime(DateTime.Now.Year, monthNumber.GetValueOrDefault(), monthDay);
+            var monthNameText = result.Substring(3);
+
+            var monthNumber = monthNameText?.GetMonthNumberFromMonthName();
+
+            var monthDay = Convert.ToInt32(monthDayText);
+
+            return new DateTime(DateTime.Now.Year, monthNumber.GetValueOrDefault(), monthDay);
+        }
     }
 
     internal static int GetMonthNumberFromMonthName(this string monthName)

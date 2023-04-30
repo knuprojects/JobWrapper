012ec1ca5caf3cf57e1643f0e6da0394e539096b
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-05-01 00:23:01 +0300

    revork protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import Login from './LogIn/Login'
 import Registration from './Registration/Registration';
 import Main from './MainPage/Main';
 import PrivateRoute from './authContext';
+
 function App() {
+  const isAuthenticated = localStorage.getItem('token') !== null;
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registration" exact element={<Registration />} />
-        <PrivateRoute path="/main" exact element={<Main />} />
+        {isAuthenticated && <Route path='/main' exact element = {<PrivateRoute component = {Main} />} />}
+        {!isAuthenticated  && <Route path="/main" element={<Login />} />} 
       </Routes>
     </div>
   );
